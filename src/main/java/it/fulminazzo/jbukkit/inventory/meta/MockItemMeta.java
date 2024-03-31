48c3ae0888f929c782444a47c5b8940f49548ec3
Fulminazzo <fulminazzo21@gmail.com>
2024-03-31 01:36:11 +0100

    MockItemMeta now extends Printable

--- src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockItemMeta.java
+++ src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockItemMeta.java
@@ -2,6 +2,7 @@ package it.fulminazzo.jbukkit.inventory.meta;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
+import it.fulminazzo.fulmicollection.objects.Printable;
 import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
 import it.fulminazzo.jbukkit.inventory.meta.tags.MockCustomItemTagContainer;
 import it.fulminazzo.jbukkit.persistence.MockPersistentDataContainer;
@@ -23,7 +24,7 @@ import java.util.stream.Collectors;
 
 @Getter
 @Setter
-public class MockItemMeta implements ItemMeta {
+public class MockItemMeta extends Printable implements ItemMeta {
     private String displayName;
     private String localizedName;
     private List<String> lore;
