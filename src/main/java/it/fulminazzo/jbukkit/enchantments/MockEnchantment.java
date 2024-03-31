17d4053764e6e9165b0ada659586780153d47b6d
Fulminazzo <fulminazzo21@gmail.com>
2024-03-31 01:26:20 +0100

    Created MockEnchantment

---
+++ src/main/java/it/fulminazzo/jbukkit/enchantments/MockEnchantment.java
@@ -0,0 +1,42 @@
+package it.fulminazzo.jbukkit.enchantments;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+@Setter
+public class MockEnchantment extends Enchantment {
+    private int startLevel;
+    private int maxLevel;
+    private EnchantmentTarget itemTarget;
+
+    public MockEnchantment(@NotNull NamespacedKey key) {
+        super(key);
+    }
+
+    @NotNull
+    public String getName() {
+        return getKey().getKey();
+    }
+
+    public boolean isTreasure() {
+        return false;
+    }
+
+    public boolean isCursed() {
+        return false;
+    }
+
+    public boolean conflictsWith(@NotNull Enchantment enchantment) {
+        return false;
+    }
+
+    public boolean canEnchantItem(@NotNull ItemStack itemStack) {
+        return this.itemTarget != null && this.itemTarget.includes(itemStack);
+    }
+}
