0148b7ebdd6e9813f2b2381096f5234751639397
Fulminazzo <fulminazzo21@gmail.com>
2024-03-31 01:26:39 +0100

    Added setupEnchantments method

--- src/main/java/it/fulminazzo/jbukkit/BukkitUtils.java
+++ src/main/java/it/fulminazzo/jbukkit/BukkitUtils.java
@@ -1,14 +1,19 @@
 package it.fulminazzo.jbukkit;
 
 import it.fulminazzo.fulmicollection.objects.Refl;
+import it.fulminazzo.jbukkit.enchantments.MockEnchantment;
 import it.fulminazzo.jbukkit.inventory.MockItemFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Server;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.Recipe;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -30,5 +35,20 @@ public class BukkitUtils {
         when(server.recipeIterator()).thenAnswer(r -> RECIPES.iterator());
         when(server.getItemFactory()).thenReturn(new MockItemFactory());
         new Refl<>(Bukkit.class).setFieldObject("server", server);
+    }
+
+    public static void setupEnchantments() {
+        List<Enchantment> enchantments = new ArrayList<>();
+        for (Field field : Enchantment.class.getDeclaredFields())
+            if (field.getType().equals(Enchantment.class))
+                try {
+                    Enchantment enchant = (Enchantment) field.get(Enchantment.class);
+                    enchantments.add(new MockEnchantment(enchant.getKey()));
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+        // Register enchantments
+        Map<NamespacedKey, Enchantment> byKey = new Refl<>(Enchantment.class).getFieldObject("byKey");
+        if (byKey != null) enchantments.forEach(e -> byKey.put(e.getKey(), e));
     }
 }
