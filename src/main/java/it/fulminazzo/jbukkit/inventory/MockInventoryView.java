5e78dd5911cea7b67e63162a485f17cfb2a74cd8
Fulminazzo <fulminazzo21@gmail.com>
2024-04-01 16:34:22 +0200

    Created MockInventoryView class

---
+++ src/main/java/it/fulminazzo/jbukkit/inventory/MockInventoryView.java
@@ -0,0 +1,43 @@
+package it.fulminazzo.jbukkit.inventory;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.PlayerInventory;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+public class MockInventoryView extends InventoryView {
+    private Inventory topInventory;
+    private final PlayerInventory bottomInventory;
+    private String title;
+    private final String originalTitle;
+
+    public MockInventoryView(PlayerInventory bottomInventory) {
+        this(bottomInventory, "");
+    }
+
+    public MockInventoryView(PlayerInventory bottomInventory, String originalTitle) {
+        this.bottomInventory = bottomInventory;
+        this.title = originalTitle;
+        this.originalTitle = originalTitle;
+    }
+
+    @NotNull
+    @Override
+    public HumanEntity getPlayer() {
+        return Objects.requireNonNull(this.bottomInventory.getHolder());
+    }
+
+    @NotNull
+    @Override
+    public InventoryType getType() {
+        return this.topInventory == null ? InventoryType.PLAYER : this.topInventory.getType();
+    }
+}
