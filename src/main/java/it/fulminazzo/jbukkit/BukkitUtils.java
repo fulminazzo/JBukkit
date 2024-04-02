e2c623563d3a0144b32dcdd11face0072976dd44
Fulminazzo <fulminazzo21@gmail.com>
2024-04-02 12:56:55 +0200

    Removed mock of getRecipe method

--- src/main/java/it/fulminazzo/jbukkit/BukkitUtils.java
+++ src/main/java/it/fulminazzo/jbukkit/BukkitUtils.java
@@ -4,7 +4,6 @@ import it.fulminazzo.fulmicollection.objects.Refl;
 import it.fulminazzo.jbukkit.enchantments.MockEnchantment;
 import it.fulminazzo.jbukkit.inventory.MockItemFactory;
 import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
 import org.bukkit.Server;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.Recipe;
@@ -13,7 +12,6 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -24,13 +22,6 @@ public class BukkitUtils {
 
     public static void setupServer() {
         Server server = mock(Server.class);
-        when(server.getRecipe(any())).thenAnswer(r -> {
-            NamespacedKey key = r.getArgument(0);
-            for (Recipe recipe : RECIPES)
-                if (key.equals(new Refl<>(recipe).getFieldObject("key")))
-                    return recipe;
-            return null;
-        });
         when(server.addRecipe(any())).thenAnswer(r -> RECIPES.add(r.getArgument(0)));
         when(server.recipeIterator()).thenAnswer(r -> RECIPES.iterator());
         when(server.getItemFactory()).thenReturn(new MockItemFactory());
