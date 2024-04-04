11bce1a2e7d1d291747614ea665e22dd27e6426d
Fulminazzo <fulminazzo21@gmail.com>
2024-04-04 18:40:55 +0200

    Added logic for detection of version from environment variable

--- src/main/java/it/fulminazzo/jbukkit/BukkitUtils.java
+++ src/main/java/it/fulminazzo/jbukkit/BukkitUtils.java
@@ -3,20 +3,42 @@ package it.fulminazzo.jbukkit;
 import it.fulminazzo.fulmicollection.objects.Refl;
 import it.fulminazzo.jbukkit.enchantments.MockEnchantment;
 import it.fulminazzo.jbukkit.inventory.MockItemFactory;
+import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.inventory.Recipe;
 
 import java.util.*;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BukkitUtils {
     private static final List<Recipe> RECIPES = new LinkedList<>();
+    private static final String VERSION_FORMAT = "1\\.(\\d+)\\.(\\d+)-R\\d+\\.\\d+-SNAPSHOT";
+    private static final String VERSION_NAME = "minecraft-version";
+    private static final String DEFAULT_VERSION = "1.20.4-R0.1-SNAPSHOT";
+    @Getter
+    private static double numericalVersion;
+
+    public static void setupVersion() {
+        String version = System.getenv(VERSION_NAME);
+        if (version == null) {
+            Logger.getGlobal().warning(String.format("Could not find set version '%s'. Defaulting to value '%s'", VERSION_NAME, DEFAULT_VERSION));
+            version = DEFAULT_VERSION;
+        }
+        Matcher matcher = Pattern.compile(VERSION_FORMAT).matcher(version);
+        if (!matcher.matches())
+            throw new IllegalArgumentException(String.format("Version '%s' did not match format '%s'", version, VERSION_FORMAT));
+        numericalVersion = Double.parseDouble(matcher.group(1) + "." + matcher.group(2));
+        Logger.getGlobal().info(String.format("Using version '%s'", numericalVersion));
+    }
 
     public static void setupServer() {
+        setupVersion();
         Server server = mock(Server.class);
         if (Arrays.stream(server.getClass().getDeclaredMethods()).anyMatch(m -> m.getName().equals("getRecipe")))
             when(server.getRecipe(any())).thenAnswer(r -> {
@@ -28,6 +50,7 @@ public class BukkitUtils {
                 }
                 return null;
             });
+        when(server.getBukkitVersion()).thenReturn(String.format("1.%s-R0.1-SNAPSHOT", numericalVersion));
         when(server.addRecipe(any())).thenAnswer(r -> RECIPES.add(r.getArgument(0)));
         when(server.recipeIterator()).thenAnswer(r -> RECIPES.iterator());
         when(server.getItemFactory()).thenReturn(new MockItemFactory());
