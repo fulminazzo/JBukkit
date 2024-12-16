5c9ac98ab34a1074dc6250aa4cfe994cb675f9a1
Fulminazzo <fulminazzo21@gmail.com>
2024-12-16 22:27:24 +0100

    Added EnchantmentUtils to support Enchantments for Minecraft 1.21 and above

---
+++ base/src/main/java/it/fulminazzo/jbukkit/utils/EnchantmentUtils.java
@@ -0,0 +1,24 @@
+package it.fulminazzo.jbukkit.utils;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
+import org.bukkit.enchantments.Enchantment;
+import org.jetbrains.annotations.NotNull;
+
+public class EnchantmentUtils {
+
+    /**
+     * Returns the enchantment corresponding to key "minecraft:{key}".
+     * If none is found, uses {@link Registry} to search for it.
+     *
+     * @param key the key
+     * @return the enchantment
+     */
+    public static Enchantment getEnchantment(final @NotNull String key) {
+        NamespacedKey actualKey = NamespacedKey.minecraft(key.toLowerCase());
+        Enchantment enchantment = Enchantment.getByKey(actualKey);
+        if (enchantment == null) enchantment = Registry.ENCHANTMENT.get(actualKey);
+        return enchantment;
+    }
+
+}
