2075953f4f1a0c954e53a0db311e1f0bf292c75c
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 23:19:25 +0100

    Created module 19 from 18

---
+++ 19/src/main/java/it/fulminazzo/jbukkit/inventory/MockInventoryView.java
@@ -0,0 +1,42 @@
+package it.fulminazzo.jbukkit.inventory;
+
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a mock implementation of {@link InventoryView}.
+ */
+@Getter
+public class MockInventoryView extends InventoryView {
+    private final @NotNull Inventory topInventory;
+    private final @NotNull Player player;
+    private final @NotNull String title;
+
+    /**
+     * Instantiates a new Mock inventory view.
+     *
+     * @param topInventory the inventory displayed
+     * @param player       the player viewing it
+     */
+    public MockInventoryView(final @NotNull Inventory topInventory, final @NotNull Player player,
+                             final @NotNull String title) {
+        this.topInventory = topInventory;
+        this.player = player;
+        this.title = title;
+    }
+
+    @Override
+    public @NotNull Inventory getBottomInventory() {
+        return this.player.getInventory();
+    }
+
+    @Override
+    public @NotNull InventoryType getType() {
+        return this.topInventory.getType();
+    }
+
+}
---
+++ 19/src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockPotionMeta.java
@@ -0,0 +1,82 @@
+package it.fulminazzo.jbukkit.inventory.meta;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.Color;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Represents a mock implementation for {@link PotionMeta}.
+ */
+@Getter
+@Setter
+public class MockPotionMeta extends MockItemMeta implements PotionMeta {
+    private final Set<PotionEffect> customEffects;
+    private PotionEffectType mainEffect;
+    private PotionData basePotionData;
+    private Color color;
+
+    /**
+     * Instantiates a new Mock potion meta.
+     */
+    public MockPotionMeta() {
+        this.customEffects = new HashSet<>();
+    }
+
+    @Override
+    public boolean hasColor() {
+        return this.color != null;
+    }
+
+    public boolean setMainEffect(PotionEffectType mainEffect) {
+        this.mainEffect = mainEffect;
+        return true;
+    }
+
+    @Override
+    public boolean hasCustomEffects() {
+        return !this.customEffects.isEmpty();
+    }
+
+    @Override
+    public List<PotionEffect> getCustomEffects() {
+        return new ArrayList<>(this.customEffects);
+    }
+
+    @Override
+    public boolean addCustomEffect(PotionEffect effect, boolean overwrite) {
+        if (hasCustomEffect(effect.getType()) && !overwrite) return false;
+        return this.customEffects.add(effect);
+    }
+
+    @Override
+    public boolean removeCustomEffect(PotionEffectType type) {
+        return this.customEffects.removeIf(p -> p.getType().equals(type));
+    }
+
+    @Override
+    public boolean hasCustomEffect(PotionEffectType type) {
+        return this.customEffects.stream().anyMatch(a -> a.getType().equals(type));
+    }
+
+    @Override
+    public boolean clearCustomEffects() {
+        if (hasCustomEffects()) return false;
+        this.customEffects.clear();
+        return true;
+    }
+
+    @Override
+    public MockPotionMeta clone() {
+        return (MockPotionMeta) super.clone();
+    }
+
+}
