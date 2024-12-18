109d1779255bc1758ea7ea61171afb0ff7aa6432
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 22:33:45 +0100

    Fixed TagContainer to support nullable value

--- 13/src/main/java/it/fulminazzo/jbukkit/inventory/meta/tags/TagContainer.java
+++ 13/src/main/java/it/fulminazzo/jbukkit/inventory/meta/tags/TagContainer.java
@@ -3,30 +3,33 @@ package it.fulminazzo.jbukkit.inventory.meta.tags;
 import lombok.Getter;
 import org.bukkit.inventory.meta.tags.ItemTagType;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 
 @Getter
 public class TagContainer<T, Z> {
-    private final ItemTagType<T, Z> type;
-    private final Z value;
+    private final @NotNull ItemTagType<T, Z> type;
+    private final @Nullable Z value;
 
     public TagContainer(final @NotNull ItemTagType<T, Z> type,
-                        final @NotNull Z value) {
+                        final @Nullable Z value) {
         this.type = Objects.requireNonNull(type);
-        this.value = Objects.requireNonNull(value);
+        this.value = value;
     }
 
     @Override
     public int hashCode() {
-        return this.type.hashCode() + this.value.hashCode();
+        int code = this.type.hashCode();
+        if (this.value != null) code += this.value.hashCode();
+        return code;
     }
 
     @Override
     public boolean equals(Object o) {
         return o instanceof TagContainer &&
                 ((TagContainer<?, ?>) o).type.equals(this.type) &&
-                ((TagContainer<?, ?>) o).value.equals(this.value);
+                Objects.equals(((TagContainer<?, ?>) o).value, this.value);
     }
 
     @Override
