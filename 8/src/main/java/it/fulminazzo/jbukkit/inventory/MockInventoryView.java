80169c76ab4ace33e748a9585008b7f5e09407da
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 11:25:58 +0100

    Added javadoc

--- 8/src/main/java/it/fulminazzo/jbukkit/inventory/MockInventoryView.java
+++ 8/src/main/java/it/fulminazzo/jbukkit/inventory/MockInventoryView.java
@@ -8,11 +8,20 @@ import org.bukkit.inventory.InventoryView;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Represents a mock implementation of {@link InventoryView}.
+ */
 @Getter
 public class MockInventoryView extends InventoryView {
     private final Inventory topInventory;
     private final Player player;
 
+    /**
+     * Instantiates a new Mock inventory view.
+     *
+     * @param topInventory the inventory displayed
+     * @param player       the player viewing it
+     */
     public MockInventoryView(final @Nullable Inventory topInventory, final @NotNull Player player) {
         this.topInventory = topInventory;
         this.player = player;
@@ -27,4 +36,5 @@ public class MockInventoryView extends InventoryView {
     public InventoryType getType() {
         return this.topInventory == null ? null : this.topInventory.getType();
     }
+
 }
