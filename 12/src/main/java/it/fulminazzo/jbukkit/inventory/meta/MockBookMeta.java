77ca3cfefb9c950bca6933232aed5a0c890d03ac
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 18:35:38 +0100

    Created MockSpigot class in MockBookMeta

--- 12/src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockBookMeta.java
+++ 12/src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockBookMeta.java
@@ -3,8 +3,10 @@ package it.fulminazzo.jbukkit.inventory.meta;
 import lombok.Getter;
 import lombok.Setter;
 import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.chat.BaseComponentSerializer;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -91,7 +93,56 @@ public class MockBookMeta extends MockItemMeta implements BookMeta {
 
     @Override
     public BookMeta.Spigot spigot() {
-        return super.spigot();
+        return new MockSpigot(this);
+    }
+
+    private static class MockSpigot extends BookMeta.Spigot {
+        private final @NotNull MockBookMeta meta;
+
+        private MockSpigot(final @NotNull MockBookMeta meta) {
+            this.meta = meta;
+        }
+
+        @Override
+        public BaseComponent[] getPage(int page) {
+            return null;
+        }
+
+        @Override
+        public void setPage(int page, BaseComponent... data) {
+            super.setPage(page, data);
+        }
+
+        @Override
+        public List<BaseComponent[]> getPages() {
+            return super.getPages();
+        }
+
+        @Override
+        public void setPages(List<BaseComponent[]> pages) {
+            super.setPages(pages);
+        }
+
+        @Override
+        public void setPages(BaseComponent[]... pages) {
+            super.setPages(pages);
+        }
+
+        @Override
+        public void addPage(BaseComponent[]... pages) {
+            super.addPage(pages);
+        }
+
+        @Override
+        public boolean isUnbreakable() {
+            return this.meta.isUnbreakable();
+        }
+
+        @Override
+        public void setUnbreakable(boolean unbreakable) {
+            this.meta.setUnbreakable(unbreakable);
+        }
+
     }
 
 }
