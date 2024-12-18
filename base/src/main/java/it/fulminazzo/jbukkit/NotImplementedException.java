e32682709edab33b4cea3aec90f81f5f5ac8af13
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 11:33:16 +0100

    Added javadoc

--- base/src/main/java/it/fulminazzo/jbukkit/NotImplementedException.java
+++ base/src/main/java/it/fulminazzo/jbukkit/NotImplementedException.java
@@ -2,6 +2,9 @@ package it.fulminazzo.jbukkit;
 
 import lombok.NoArgsConstructor;
 
+/**
+ * Exception thrown as placeholder for not implemented methods.
+ */
 @NoArgsConstructor
 public final class NotImplementedException extends RuntimeException {
 
