7a053de560549346067247ed861e4333b2b49ecf
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 02:58:10 +0100

    Created MockBookMeta class

---
+++ 8/src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockBookMeta.java
@@ -0,0 +1,73 @@
+package it.fulminazzo.jbukkit.inventory.meta;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.inventory.meta.BookMeta;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+@Getter
+@Setter
+public class MockBookMeta extends MockItemMeta implements BookMeta {
+    private final List<String> pages;
+    private String title;
+    private String author;
+
+    public MockBookMeta() {
+        this.pages = new LinkedList<>();
+    }
+
+    @Override
+    public boolean hasTitle() {
+        return this.title != null;
+    }
+
+    @Override
+    public boolean hasAuthor() {
+        return this.author != null;
+    }
+
+    @Override
+    public boolean hasPages() {
+        return !this.pages.isEmpty();
+    }
+
+    @Override
+    public String getPage(int page) {
+        return this.pages.get(page);
+    }
+
+    @Override
+    public void setPage(int page, String data) {
+        this.pages.set(page, data);
+    }
+
+    @Override
+    public void setPages(List<String> pages) {
+        this.pages.clear();
+        this.pages.addAll(pages);
+    }
+
+    @Override
+    public void setPages(String... pages) {
+        setPages(Arrays.asList(pages));
+    }
+
+    @Override
+    public void addPage(String... pages) {
+        this.pages.addAll(Arrays.asList(pages));
+    }
+
+    @Override
+    public int getPageCount() {
+        return this.pages.size();
+    }
+
+    @Override
+    public MockBookMeta clone() {
+        return (MockBookMeta) super.clone();
+    }
+
+}
