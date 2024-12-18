04f6d6bd66164d73fbb59ea90f14998eb804c803
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 22:49:31 +0100

    Created module 14 from 13

---
+++ 14/src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockBlockStateMeta.java
@@ -0,0 +1,26 @@
+package it.fulminazzo.jbukkit.inventory.meta;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.block.BlockState;
+import org.bukkit.inventory.meta.BlockStateMeta;
+
+/**
+ * Represents a mock implementation for {@link BlockStateMeta}.
+ */
+@Getter
+@Setter
+public class MockBlockStateMeta extends MockItemMeta implements BlockStateMeta {
+    private BlockState blockState;
+
+    @Override
+    public boolean hasBlockState() {
+        return this.blockState != null;
+    }
+
+    @Override
+    public MockBlockStateMeta clone() {
+        return (MockBlockStateMeta) super.clone();
+    }
+
+}
---
+++ 14/src/main/java/it/fulminazzo/jbukkit/inventory/meta/tags/MockCustomItemTagContainer.java
@@ -0,0 +1,54 @@
+package it.fulminazzo.jbukkit.inventory.meta.tags;
+
+import lombok.Getter;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.meta.tags.CustomItemTagContainer;
+import org.bukkit.inventory.meta.tags.ItemTagAdapterContext;
+import org.bukkit.inventory.meta.tags.ItemTagType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class MockCustomItemTagContainer implements CustomItemTagContainer {
+    private final Map<NamespacedKey, TagContainer<?, ?>> customDataTags;
+    private final ItemTagAdapterContext adapterContext;
+
+    public MockCustomItemTagContainer() {
+        this.customDataTags = new HashMap<>();
+        this.adapterContext = new MockItemTagAdapterContext();
+    }
+
+    @Override
+    public <T, Z> void setCustomTag(@NotNull NamespacedKey key, @NotNull ItemTagType<T, Z> type, @NotNull Z value) {
+        this.customDataTags.put(key, new TagContainer<>(type, value));
+    }
+
+    @Override
+    public <T, Z> boolean hasCustomTag(@NotNull NamespacedKey key, @NotNull ItemTagType<T, Z> type) {
+        return getCustomTag(key, type) != null;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public @Nullable <T, Z> Z getCustomTag(@NotNull NamespacedKey key, @NotNull ItemTagType<T, Z> type) {
+        TagContainer<?, ?> tagContainer = this.customDataTags.get(key);
+        if (tagContainer == null) return null;
+        Object value = tagContainer.getValue();
+        if (value == null) return null;
+        return type.fromPrimitive((T) value, this.adapterContext);
+    }
+
+    @Override
+    public void removeCustomTag(@NotNull NamespacedKey key) {
+        this.customDataTags.remove(key);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.customDataTags.isEmpty();
+    }
+
+}
