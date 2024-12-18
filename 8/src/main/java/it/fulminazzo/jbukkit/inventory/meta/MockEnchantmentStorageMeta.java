a5486b46492fd7e9a2abc7bf494382d3569af090
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 03:00:05 +0100

    Created MockEnchantmentStorageMeta class

---
+++ 8/src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockEnchantmentStorageMeta.java
@@ -0,0 +1,57 @@
+package it.fulminazzo.jbukkit.inventory.meta;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+public class MockEnchantmentStorageMeta extends MockItemMeta implements EnchantmentStorageMeta {
+    private final Map<Enchantment, Integer> storedEnchants;
+
+    public MockEnchantmentStorageMeta() {
+        this.storedEnchants = new HashMap<>();
+    }
+
+    @Override
+    public boolean hasStoredEnchants() {
+        return !storedEnchants.isEmpty();
+    }
+
+    @Override
+    public boolean hasStoredEnchant(Enchantment ench) {
+        return this.storedEnchants.containsKey(ench);
+    }
+
+    @Override
+    public int getStoredEnchantLevel(Enchantment ench) {
+        return this.storedEnchants.get(ench);
+    }
+
+    @Override
+    public boolean addStoredEnchant(Enchantment ench, int level, boolean ignoreLevelRestriction) {
+        if (!ignoreLevelRestriction && ench.getMaxLevel() < level) return false;
+        this.storedEnchants.put(ench, level);
+        return true;
+    }
+
+    @Override
+    public boolean removeStoredEnchant(Enchantment ench) throws IllegalArgumentException {
+        return this.storedEnchants.remove(ench) != null;
+    }
+
+    @Override
+    public boolean hasConflictingStoredEnchant(Enchantment ench) {
+        return this.storedEnchants.keySet().stream().anyMatch(e -> e.conflictsWith(ench));
+    }
+
+    @Override
+    public MockEnchantmentStorageMeta clone() {
+        return (MockEnchantmentStorageMeta) super.clone();
+    }
+
+}
