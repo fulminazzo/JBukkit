3e4ff8f7d85ee5a3cb61ae3f76b8feb00fd5db13
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 01:58:37 +0100

    Created MockEnchantment class

---
+++ base/src/main/java/it/fulminazzo/jbukkit/enchantments/MockEnchantment.java
@@ -0,0 +1,9 @@
+package it.fulminazzo.jbukkit.enchantments;
+
+//TODO: Mock class, should be implemented by single modules
+public class MockEnchantment {
+
+    public static void setupEnchantments() {
+
+    }
+}
