a22006979e26837100a5017835f1d49c4064638d
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 22:15:55 +0100

    Created module 13 from 12

---
+++ 13/src/main/java/it/fulminazzo/jbukkit/enchantments/MockEnchantment.java
@@ -0,0 +1,129 @@
+package it.fulminazzo.jbukkit.enchantments;
+
+import it.fulminazzo.fulmicollection.objects.Refl;
+import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
+import lombok.Getter;
+import lombok.SneakyThrows;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Represents an implementation of {@link Enchantment}.
+ */
+@Getter
+public class MockEnchantment extends Enchantment {
+    private final int id;
+    private final String name;
+    private final int startLevel;
+    private final int maxLevel;
+    private final EnchantmentTarget itemTarget;
+    private final Set<Enchantment> conflicts;
+    private final Set<ItemStack> canEnchantItems;
+    private boolean treasure;
+    private boolean cursed;
+
+    /**
+     * Instantiates a new Mock enchantment.
+     * It will copy each value from the given enchantment.
+     *
+     * @param enchantment the enchantment
+     */
+    public MockEnchantment(final @NotNull Enchantment enchantment) {
+        this(enchantment.getId(), enchantment.getName(), enchantment.getStartLevel(),
+                enchantment.getMaxLevel(), enchantment.getItemTarget());
+        setTreasure(enchantment.isTreasure()).setCursed(enchantment.isCursed());
+    }
+
+    /**
+     * Instantiates a new Mock enchantment.
+     *
+     * @param id         the id
+     * @param name       the name
+     * @param startLevel the start level
+     * @param maxLevel   the max level
+     * @param itemTarget the item target
+     */
+    public MockEnchantment(final int id, final @NotNull String name, final int startLevel,
+                           final int maxLevel, final @NotNull EnchantmentTarget itemTarget) {
+        super(id);
+        this.id = id;
+        this.name = name;
+        this.startLevel = startLevel;
+        this.maxLevel = maxLevel;
+        this.itemTarget = itemTarget;
+        this.conflicts = new HashSet<>();
+        this.canEnchantItems = new HashSet<>();
+    }
+
+    public @NotNull MockEnchantment setTreasure(boolean treasure) {
+        this.treasure = treasure;
+        return this;
+    }
+
+    public @NotNull MockEnchantment setCursed(boolean cursed) {
+        this.cursed = cursed;
+        return this;
+    }
+
+    /**
+     * Explicitly declare the enchantments with which this enchantment will conflict.
+     *
+     * @param conflicts the enchantments
+     * @return this enchantment
+     */
+    public @NotNull MockEnchantment conflictsWith(final Enchantment @NotNull ... conflicts) {
+        this.conflicts.addAll(Arrays.asList(conflicts));
+        return this;
+    }
+
+    /**
+     * Explicitly declare the items which this enchantment can be applied to.
+     *
+     * @param canEnchantItems the items
+     * @return this enchantment
+     */
+    public @NotNull MockEnchantment canEnchantItems(final @NotNull ItemStack... canEnchantItems) {
+        this.canEnchantItems.addAll(Arrays.asList(canEnchantItems));
+        return this;
+    }
+
+    @Override
+    public boolean conflictsWith(final @NotNull Enchantment other) {
+        return this.conflicts.contains(other);
+    }
+
+    @Override
+    public boolean canEnchantItem(final @NotNull ItemStack item) {
+        return this.canEnchantItems.contains(item);
+    }
+
+    /**
+     * Sets up the default vanilla enchantments by checking the {@link Enchantment} static fields.
+     */
+    @SneakyThrows
+    public static void setupEnchantments() {
+        Refl<Class<Enchantment>> enchantmentClass = new Refl<>(Enchantment.class);
+        Map<Integer, Enchantment> byId = enchantmentClass.getFieldObject("byId");
+        Map<String, Enchantment> byName = enchantmentClass.getFieldObject("byName");
+        byId.clear();
+        byName.clear();
+        for (Field field : enchantmentClass.getStaticFields())
+            if (field.getType().equals(Enchantment.class)) {
+                Enchantment enchantment = (Enchantment) ReflectionUtils.setAccessibleOrThrow(field)
+                        .get(Enchantment.class);
+                MockEnchantment mock = new MockEnchantment(enchantment);
+                field.set(Enchantment.class, mock);
+                byId.put(mock.getId(), enchantment);
+                byName.put(mock.getName(), enchantment);
+            }
+    }
+
+}
