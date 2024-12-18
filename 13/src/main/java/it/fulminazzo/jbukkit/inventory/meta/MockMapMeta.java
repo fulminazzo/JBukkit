624b02f2df8736677b551c2a05fedaf39accbece
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 22:44:57 +0100

    Added mapId and mapView fields

--- 13/src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockMapMeta.java
+++ 13/src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockMapMeta.java
@@ -1,9 +1,11 @@
 package it.fulminazzo.jbukkit.inventory.meta;
 
+import it.fulminazzo.jbukkit.map.MockMapView;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Color;
 import org.bukkit.inventory.meta.MapMeta;
+import org.bukkit.map.MapView;
 
 /**
  * Represents a mock implementation for {@link MapMeta}.
@@ -14,10 +16,27 @@ public class MockMapMeta extends MockItemMeta implements MapMeta {
     private boolean scaling;
     private String locationName;
     private Color color;
+    private Integer mapId;
+    private MapView mapView;
 
     @Override
     public MockMapMeta clone() {
         return (MockMapMeta) super.clone();
+    }
+
+    @Override
+    public boolean hasMapId() {
+        return this.mapId != null;
+    }
+
+    @Override
+    public void setMapId(int id) {
+        this.mapId = id;
+    }
+
+    @Override
+    public boolean hasMapView() {
+        return this.mapView != null;
     }
 
     @Override
