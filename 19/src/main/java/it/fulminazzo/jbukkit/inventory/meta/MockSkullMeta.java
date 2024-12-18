e0f93fc3107265bbeb0307741b1c7330ba063ebd
Fulminazzo <fulminazzo21@gmail.com>
2024-12-18 23:20:48 +0100

    Added noteBlockSound field

--- 19/src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockSkullMeta.java
+++ 19/src/main/java/it/fulminazzo/jbukkit/inventory/meta/MockSkullMeta.java
@@ -2,6 +2,7 @@ package it.fulminazzo.jbukkit.inventory.meta;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.NamespacedKey;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.profile.PlayerProfile;
@@ -15,6 +16,7 @@ public class MockSkullMeta extends MockItemMeta implements SkullMeta {
     private String owner;
     private OfflinePlayer owningPlayer;
     private PlayerProfile ownerProfile;
+    private NamespacedKey noteBlockSound;
 
     public boolean setOwner(String owner) {
         this.owner = owner;
