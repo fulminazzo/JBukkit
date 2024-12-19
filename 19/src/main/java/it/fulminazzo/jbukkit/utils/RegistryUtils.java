89b15ea32d50ac79894dc31869e08dd1ea13da45
Fulminazzo <fulminazzo21@gmail.com>
2024-12-19 15:42:02 +0100

    Copied RegistryUtils class in module 19

---
+++ 19/src/main/java/it/fulminazzo/jbukkit/utils/RegistryUtils.java
@@ -0,0 +1,19 @@
+package it.fulminazzo.jbukkit.utils;
+
+import lombok.NoArgsConstructor;
+import org.bukkit.Registry;
+
+/**
+ * Utility class for {@link Registry}.
+ */
+@NoArgsConstructor
+public final class RegistryUtils {
+
+    /**
+     * Sets up the registries.
+     */
+    public static void setupRegistries() {
+
+    }
+
+}
