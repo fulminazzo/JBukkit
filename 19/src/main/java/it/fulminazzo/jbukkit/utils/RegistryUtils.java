830cc0c856273db6a0403618612368cc4f3a0dc1
Fulminazzo <fulminazzo21@gmail.com>
2024-12-19 15:51:00 +0100

    Implemented setupRegistry function using getRegistry internal function

--- 19/src/main/java/it/fulminazzo/jbukkit/utils/RegistryUtils.java
+++ 19/src/main/java/it/fulminazzo/jbukkit/utils/RegistryUtils.java
@@ -1,7 +1,13 @@
 package it.fulminazzo.jbukkit.utils;
 
 import lombok.NoArgsConstructor;
+import org.bukkit.Bukkit;
+import org.bukkit.Keyed;
 import org.bukkit.Registry;
+import org.jetbrains.annotations.NotNull;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * Utility class for {@link Registry}.
@@ -13,7 +19,8 @@ public final class RegistryUtils {
      * Sets up the registries.
      */
     public static void setupRegistries() {
-
+        when(Bukkit.getServer().getRegistry(any())).thenAnswer(a ->
+                getRegistry(a.getArgument(0)));
     }
 
 }
