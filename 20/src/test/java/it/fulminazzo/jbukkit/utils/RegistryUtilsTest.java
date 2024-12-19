e747c4ce528a40e259ca07635a09801628bbf236
Fulminazzo <fulminazzo21@gmail.com>
2024-12-19 17:59:13 +0100

    Added tests for fields registry

--- 20/src/test/java/it/fulminazzo/jbukkit/utils/RegistryUtilsTest.java
+++ 20/src/test/java/it/fulminazzo/jbukkit/utils/RegistryUtilsTest.java
@@ -2,19 +2,24 @@ package it.fulminazzo.jbukkit.utils;
 
 import it.fulminazzo.fulmicollection.objects.Refl;
 import it.fulminazzo.jbukkit.BukkitUtils;
+import it.fulminazzo.jbukkit.registries.FieldsRegistry;
 import org.bukkit.*;
 import org.bukkit.advancement.Advancement;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Biome;
 import org.bukkit.block.banner.PatternType;
 import org.bukkit.boss.KeyedBossBar;
-import org.bukkit.entity.Cat;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Frog;
-import org.bukkit.entity.Villager;
+import org.bukkit.damage.DamageType;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.*;
 import org.bukkit.entity.memory.MemoryKey;
+import org.bukkit.generator.structure.Structure;
+import org.bukkit.generator.structure.StructureType;
+import org.bukkit.inventory.meta.trim.TrimMaterial;
+import org.bukkit.inventory.meta.trim.TrimPattern;
 import org.bukkit.loot.LootTables;
 import org.bukkit.map.MapCursor;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.potion.PotionType;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -26,6 +31,17 @@ class RegistryUtilsTest {
     private static Object[][] getRegistries() {
         BukkitUtils.setupServer();
         return new Object[][] {
+                // Fields Registry
+                new Object[]{GameEvent.class, FieldsRegistry.class},
+                new Object[]{Wolf.Variant.class, FieldsRegistry.class},
+                new Object[]{DamageType.class, FieldsRegistry.class},
+                new Object[]{TrimPattern.class, FieldsRegistry.class},
+                new Object[]{TrimMaterial.class, FieldsRegistry.class},
+                new Object[]{StructureType.class, FieldsRegistry.class},
+                new Object[]{Structure.class, FieldsRegistry.class},
+                new Object[]{PotionEffectType.class, FieldsRegistry.class},
+                new Object[]{MusicInstrument.class, FieldsRegistry.class},
+                new Object[]{Enchantment.class, FieldsRegistry.class},
                 // Abstract Registry
                 new Object[]{Advancement.class, Registry.class},
                 new Object[]{KeyedBossBar.class, Registry.class},
