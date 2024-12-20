e5429d4459f4397b7cfe0ce131859e75395cde88
Fulminazzo <fulminazzo21@gmail.com>
2024-12-20 10:48:31 +0100

    Added checkPrimitive method

--- base/src/main/java/it/fulminazzo/jbukkit/Equable.java
+++ base/src/main/java/it/fulminazzo/jbukkit/Equable.java
@@ -3,6 +3,7 @@ package it.fulminazzo.jbukkit;
 import it.fulminazzo.fulmicollection.objects.Printable;
 import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -10,6 +11,27 @@ import org.jetbrains.annotations.Nullable;
  */
 @NoArgsConstructor
 public class Equable extends Printable {
+
+    /**
+     * Verifies that the given object is of primitive type.
+     * If it is, verifies that its value is 0.
+     *
+     * @param type   the type
+     * @param object the object
+     * @return true if it is
+     */
+    boolean checkPrimitive(final @NotNull Class<?> type, final @NotNull Object object) {
+        if (!ReflectionUtils.isPrimitive(type)) return false;
+        if (type.equals(boolean.class)) return !((boolean) object);
+        else if (type.equals(byte.class)) return ((byte) object) == 0;
+        else if (type.equals(short.class)) return ((short) object) == 0;
+        else if (type.equals(char.class)) return ((char) object) == 0;
+        else if (type.equals(int.class)) return ((int) object) == 0;
+        else if (type.equals(long.class)) return ((long) object) == 0L;
+        else if (type.equals(float.class)) return ((float) object) == 0.0f;
+        else if (type.equals(double.class)) return ((double) object) == 0.0d;
+        return false;
+    }
 
     @Override
     public boolean equals(Object o) {
