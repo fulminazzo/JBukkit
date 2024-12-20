6ffb7959cd189ee996143e5cce1aaee5e2872a1d
Fulminazzo <fulminazzo21@gmail.com>
2024-12-20 10:08:46 +0100

    Made AdapterContexts extend Equable

--- 13/src/main/java/it/fulminazzo/jbukkit/inventory/meta/tags/MockItemTagAdapterContext.java
+++ 13/src/main/java/it/fulminazzo/jbukkit/inventory/meta/tags/MockItemTagAdapterContext.java
@@ -1,5 +1,6 @@
 package it.fulminazzo.jbukkit.inventory.meta.tags;
 
+import it.fulminazzo.jbukkit.Equable;
 import org.bukkit.inventory.meta.tags.CustomItemTagContainer;
 import org.bukkit.inventory.meta.tags.ItemTagAdapterContext;
 import org.jetbrains.annotations.NotNull;
@@ -7,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
 /**
  * Represents a mock for {@link ItemTagAdapterContext}.
  */
-public class MockItemTagAdapterContext implements ItemTagAdapterContext {
+public class MockItemTagAdapterContext extends Equable implements ItemTagAdapterContext {
 
     @Override
     public @NotNull CustomItemTagContainer newTagContainer() {
--- 14/src/main/java/it/fulminazzo/jbukkit/persistence/MockPersistentDataAdapterContext.java
+++ 14/src/main/java/it/fulminazzo/jbukkit/persistence/MockPersistentDataAdapterContext.java
@@ -1,5 +1,6 @@
 package it.fulminazzo.jbukkit.persistence;
 
+import it.fulminazzo.jbukkit.Equable;
 import org.bukkit.persistence.PersistentDataAdapterContext;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.jetbrains.annotations.NotNull;
@@ -7,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
 /**
  * Represents a mock for {@link PersistentDataAdapterContext}.
  */
-public class MockPersistentDataAdapterContext implements PersistentDataAdapterContext {
+public class MockPersistentDataAdapterContext extends Equable implements PersistentDataAdapterContext {
 
     @Override
     public @NotNull PersistentDataContainer newPersistentDataContainer() {
