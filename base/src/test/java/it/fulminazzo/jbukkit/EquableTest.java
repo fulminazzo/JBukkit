f99a0244ee2669f4b84039818e1f7629a524de69
Fulminazzo <fulminazzo21@gmail.com>
2024-12-20 10:47:41 +0100

    Added tests for Equable#checkPrimitive

--- base/src/test/java/it/fulminazzo/jbukkit/EquableTest.java
+++ base/src/test/java/it/fulminazzo/jbukkit/EquableTest.java
@@ -1,6 +1,8 @@
 package it.fulminazzo.jbukkit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.*;
 
@@ -12,6 +14,26 @@ class EquableTest {
     void testCompareNull() {
         assertTrue(new MockClass().compareNull(),
                 "When comparing with null should return true");
+    }
+
+    private static Object[][] getPrimitives() {
+        return new Object[][] {
+                new Object[] {boolean.class, false},
+                new Object[] {byte.class, ((byte) 0)},
+                new Object[] {short.class, ((short) 0)},
+                new Object[] {char.class, ((char) 0)},
+                new Object[] {int.class, 0},
+                new Object[] {long.class, 0L},
+                new Object[] {float.class, 0.0F},
+                new Object[] {double.class, 0.0D},
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("getPrimitives")
+    void testCheckPrimitive(Class<?> type, Object value) {
+        assertTrue(new MockClass().checkPrimitive(type, value),
+                String.format("%s of value %s should have returned true", type, value));
     }
 
     @SuppressWarnings("unused")
