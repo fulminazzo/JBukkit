9cde17fd9aa393d60268996f4dd4d6e4996f772e
Fulminazzo <fulminazzo21@gmail.com>
2024-12-24 12:42:18 +0100

    !release Totally reworked project with **multiple modules** for **each Minecraft version**. Added missing `UnsafeValues` implementation. Added basic implementation for `BlockType` and `ItemType` registries. Added module **21** for support of **Minecraft 1.21**. Added `MockInventoryView` class to **base** module for compatibility reasons. Rewritten and updated README.md file. Updated `FieldsRegistry#internalFunction` type to binary function. Updated `BukkitUtils#setupServer` to check if server is above version **14**. Updated Minecraft **1.20** Java version to **17**. Updated Fulminazzo dependencies to latest versions. Implemented `server#getRecipesFor` method. Implemented `name()` method for Keyed objects returned by FieldsRegistries. Fixed many `PotionEffectType` related methods for versions older than **1.19**. Fixed `MockEnchantment` initialization error. Fixed `MockItemFactory#getItemMeta` not returning `null` for `Material#AIR`. Fixed `Equable#compareNull` function. Fixed `MockUnsafeValues#toLegacy` and `MockUnsafeValues#fromLegacy` methods. Fixed `RegistryUtils` in module **21**. Fixed `FieldsRegistry#checkInternalMap` and `FieldsRegistry#get` methods. Fixed `Sound` registry not working properly.

---
+++ base/src/main/java/it/fulminazzo/jbukkit/inventory/MockInventoryView.java
@@ -0,0 +1,47 @@
+package it.fulminazzo.jbukkit.inventory;
+
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
+import org.jetbrains.annotations.NotNull;
+
+import static org.mockito.Mockito.when;
+
+/**
+ * Represents a mock implementation of {@link InventoryView}.
+ */
+@Getter
+public class MockInventoryView extends InventoryView {
+    private final @NotNull Inventory topInventory;
+    private final @NotNull Player player;
+    private final @NotNull String title;
+
+    /**
+     * Instantiates a new Mock inventory view.
+     *
+     * @param topInventory the inventory displayed
+     * @param player       the player viewing it
+     * @param title        the title
+     */
+    public MockInventoryView(final @NotNull Inventory topInventory, final @NotNull Player player,
+                             final @NotNull String title) {
+        this.topInventory = topInventory;
+        this.player = player;
+        this.title = title;
+        if (player.getClass().getCanonicalName().contains("MockitoMock"))
+            when(player.getOpenInventory()).thenReturn(this);
+    }
+
+    @Override
+    public @NotNull Inventory getBottomInventory() {
+        return this.player.getInventory();
+    }
+
+    @Override
+    public @NotNull InventoryType getType() {
+        return this.topInventory.getType();
+    }
+
+}
